a4c3dbffcd92abaf67c24005d9f30e1f3c1991ae
Will <weosborne90@gmail.com>
2024-02-29 11:39:51 -0500

    Getting started, creating python tools to prepare my existing data to write to a postgres database to be used in the rest of the application.

---
+++ Python/main.py
@@ -0,0 +1,16 @@
+# This will be used to read my existing data and populate a postgres database
+# I am doing most of the pandas in jupyter notebook at the moment but this would
+# be a good place to create an import tool later as well
+import psycopg
+import pandas as pd
+
+
+def read_spreadsheet():
+    pd.read_excel('')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('main')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
